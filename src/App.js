425e8d1d499349c57d995446ee5aae257b4e1624
daniil203 <70706736+daniil203@users.noreply.github.com>
2024-08-11 22:44:39 +0300

    перенёс формирование pdf на фронтенд

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Div, Textarea, Button } from '@vkontakte/vkui';
-import '@vkontakte/vkui/dist/vkui.css';
+import { createPdf } from './utils'
 
 const PDFGenerator = () => {
   const [content, setContent] = useState('');
 
-  axios.defaults.baseURL = 'http://localhost:3001'; // или URL вашего развернутого сервера
+  // axios.defaults.baseURL = 'http://localhost:3001'; // или URL вашего развернутого сервера
 
-  useEffect(() => {
-    const callBackendAPI = async () => {
-      try {
-        // Эта функция не используется, поэтому ее можно убрать
-        // const response = await axios.get('/initial-data');
-        // setPdfData(response.data);
-        // console.log('Initial data received:', response.data);
-      } catch (error) {
-        console.error('Error fetching initial data:', error);
-        alert('Error fetching initial data. Please try again later.');
-      }
-    };
+  // useEffect(() => {
+  //   const callBackendAPI = async () => {
+  //     try {
+  //       // Эта функция не используется, поэтому ее можно убрать
+  //       // const response = await axios.get('/initial-data');
+  //       // setPdfData(response.data);
+  //       // console.log('Initial data received:', response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching initial data:', error);
+  //       alert('Error fetching initial data. Please try again later.');
+  //     }
+  //   };
 
-    callBackendAPI();
-  }, []);
+  //   callBackendAPI();
+  // }, []);
 
-  const generatePDF = async () => {
-    try {
-      const response = await axios.post('/generate-pdf', { content });
-      const blob = new Blob([response.data], { type: 'application/pdf' });
-      const url = URL.createObjectURL(blob);
-      window.open(url);
-    } catch (error) {
-      console.error('Error generating PDF:', error);
-      alert('Error generating PDF. Please try again later.');
-    }
-  };
+  // const generatePDF = async () => {
+  //   try {
+  //     const response = await axios.post('/generate-pdf', { content });
+  //     // const blob = new Blob([response.data], { type: 'application/pdf' });
+  //     // const url = URL.createObjectURL(blob);
+  //     console.log("Что пришло с сервера", response)
+  //     window.open(url);
+  //   } catch (error) {
+  //     console.error('Error generating PDF:', error);
+  //     alert('Error generating PDF. Please try again later.');
+  //   }
+  // };
+
+  const generatePDF = () => {
+    if(content.length) createPdf({ content: [{ text: content, style: 'header' }] });
+    else alert("Пожалуйста, введите текст для преобразования.")
+  }
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import bridge from '@vkontakte/vk-bridge';
+import '@vkontakte/vkui/dist/vkui.css';
 
 bridge.send('VKWebAppInit');
 
